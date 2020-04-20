5b3f82492e0e83b86523b2cd1f774b3ea3b282d2
Elenadisa <Elenadisa@users.noreply.github.com>
2020-04-20 10:51:43 +0200

    function.py

--- scripts/py_scripts/functions.py
+++ scripts/py_scripts/functions.py
@@ -18,3 +18,33 @@ def build_dictionary(filename, key_col_number, value_col_number):
 			dictionary[key].append(value)
 
 	return dictionary
+
+
+	
+	def build_list(filename, col, tab, duplicate):
+	l = list()
+	file = open(filename)
+	
+	for line in file:
+		line = line.rstrip("\n")
+		fields = line.split(tab)
+		element = fields[col]
+
+		if duplicate == False:
+			if element not in l:
+				l.append(element)
+		else:
+			l.append(element)
+
+	return(l)
+
+
+
+def load_list_from_a_file(filename):
+	l = list()
+	file = open(filename)
+	for line in file:
+		line = line.rstrip("\n")
+		l.append(line)
+
+	return(l)
